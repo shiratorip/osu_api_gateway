13cd02f40e76fbe564ec9d8dfa6996d4e90045a3
shiratorip <kirillbabko.bda@gmail.com>
2023-11-18 15:07:33 +0100

    updated main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ load_dotenv()
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-
 app = Flask(__name__)
 
 
@@ -18,10 +17,13 @@ def search():
     wrapper = ApiWrapper(CLIENT_ID, CLIENT_SECRET)
     query = request.args.get('query')
 
-
     if not query:
         return {"error": "Query not provided"}
 
     users = wrapper.search_users(query)
 
-    return {"result": {user.id: user.username for user in users}}
+    return {"result": {
+        "avatar_url": user.avatar_url,
+        "id": user.id,
+        "username": user.username
+    } for user in users}
